3605c8c48b736bc14e4093352748bde0f1d7f514
Aurelio scarante taborda <aurelio@Aurelio>
2023-05-12 14:28:02 -0300

    Criando função que calcula a area de um triangulo

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ function compareTrue() {
 console.log(compareTrue())
 
 //Requisito - 2
-const base = 2;
-const height = 4;
-function calcArea(base , height) {
-  const area = (base * height) / 2
-  return area
+
+function calcArea() {
+    const base = 2;
+    const height = 4;
+    const area = (base * height) / 2
+    return area
 }
 
-console.log(calcArea);
-
-console.log(a > 0 && b > 0);
+console.log(calcArea());
